f7b5eae6f27959d17e2415eb921311402ca75320
akrupych <akrupych@remit.se>
2018-04-24 18:05:04 +0300

    firebase DB

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,39 @@
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(new MyApp());
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Flitter',
-      theme: new ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: new MyHomePage(title: 'Flitter'),
-    );
-  }
+void main() async {
+  runApp(new MaterialApp(
+    title: 'Flitter',
+    home: new MyHomePage(),
+  ));
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
   @override
   _MyHomePageState createState() => new _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  DatabaseReference messages =
+      FirebaseDatabase.instance.reference().child("messages");
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-        appBar: new AppBar(
-          title: new Text(widget.title),
-        ),
-        body: new Container());
+      appBar: new AppBar(
+        title: new Text("Flitter"),
+      ),
+      body: new FirebaseAnimatedList(
+          query: messages,
+          itemBuilder: (BuildContext context, DataSnapshot snapshot,
+                  Animation<double> animation, int index) =>
+              new SizeTransition(
+                sizeFactor: animation,
+                child: new ListTile(
+                    title: new Text(
+                        "${snapshot.key}: ${snapshot.value.toString()}")),
+              )),
+    );
   }
 }
